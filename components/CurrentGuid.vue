64a6c2dd14cdbb56bd80ac185c11c24474d2d821
luke-gibson <hello@lukegibsondesign.co.uk>
2025-05-02 11:37:28 +0100

    fix: improve alert styling for copied GUID notification

--- components/CurrentGuid.vue
+++ components/CurrentGuid.vue
@@ -29,8 +29,8 @@
       >
         <IconsCopyIcon class="w-4 h-4 sm:mr-2" /> <span class="hidden sm:block">Copy</span>
       </button>
-    </div>
-    <div v-if="copied"  class="flex items-center p-4 mb-4 text-[#0D162B] bg-white bg-opacity-80 rounded-lg shadow-sm fixed top-2 right-2" role="alert">
+    </div>    
+      <div v-if="copied" class="flex items-center py-2 px-6 mb-4 text-[#0D162B] bg-white bg-opacity-80 rounded-full shadow-sm fixed top-2 right-2 transition-all" role="alert">
       <div class="inline-flex items-center justify-center shrink-0 w-8 h-8 text-white bg-[#00DC82] rounded-lg">
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
